31acbce10b0bed0932a71c9c9121b6803955a6f5
Jakl0 <uT_3999@zsp3zamosc.pl>
2025-11-04 12:57:48 +0100

    dodano zadanie 1

---
+++ consoleapp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 4;
+            Console.WriteLine($"Liczba a = {a}");
+            Console.WriteLine($"Liczba b = {b}");
+            Console.WriteLine($"Sumba: {a+b}");
+            Console.WriteLine($"Różnica: {a - b}");
+            Console.WriteLine($"Iloczyn: {a * b}");
+            Console.WriteLine($"Iloraz: {a / b}");
+            Console.WriteLine($"Reszta z dzielenia: {a % b}");
+
+        }
+    }
+}
